2ea66d3ce9d29d26062e9b715cf6a3f970308812
Sohan Pulluru <sohanpulluru@gmail.com>
2023-03-08 18:26:52 +0530

    Create Max min Height gfg.cpp

---
+++ MAR 2023/08 Mar/Max min Height gfg.cpp
@@ -0,0 +1,50 @@
+class Solution
+{
+    public:
+        bool isOK(vector<int> &arr, int days, int cs, long long int h)
+        {
+            int n=arr.size();
+            vector<long long> waterSupply(n, 0);
+            if(arr[0]<h)
+            {
+                waterSupply[0] = h-arr[0];
+                days = days - (h-arr[0]);
+            }
+            
+            if(days < 0) return false;
+            
+            for(int i = 1; i < arr.size(); i++)
+            {
+                waterSupply[i] = waterSupply[i-1];
+                int actualHeight = arr[i];
+                if(i>=cs) actualHeight += (waterSupply[i]-waterSupply[i-cs]);
+                else actualHeight += waterSupply[i];
+    
+                if(actualHeight < h)
+                {
+                    waterSupply[i] += (h-actualHeight);
+                    days -=( h - actualHeight);
+                }
+                if(days < 0) return false;
+            }
+            return true;
+        }
+        
+        long long maximizeMinHeight(int n, int k, int w, vector<int> &a)
+        {
+            long long res = -1;
+            long long mnHeight = *min_element(a.begin(),a.end());
+            long long mxHeight = INT_MAX/2;
+            while(mnHeight <= mxHeight)
+            {
+                int guessHeight = (mxHeight+mnHeight)/2;
+                if(isOK(a,k,w,guessHeight)==true)
+                {
+                    res = guessHeight;
+                    mnHeight = guessHeight+1;
+                }
+                else mxHeight = guessHeight-1;
+            }
+            return res;
+        }
+};
