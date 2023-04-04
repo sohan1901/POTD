4d4797d7dd173e8a63e1e6fc8c0c48c8d2243cc8
Sohan Pulluru <sohanpulluru@gmail.com>
2023-04-04 09:57:03 +0530

    Update 2405. Optimal Partition of String LC.cpp

--- APR 2023/04 Apr/2405. Optimal Partition of String LC.cpp
+++ APR 2023/04 Apr/2405. Optimal Partition of String LC.cpp
@@ -4,15 +4,11 @@ public:
         int n = s.size();
         int i = 0;
         int ans = 0;
-        char ch = s[0];
         while(i < n)
         {
             unordered_map<char, int> m;
             while(i < n && m.find(s[i]) == m.end())
-            {
                 m[s[i++]]++;
-                ch = s[i];
-            }
             ans++;
         }
         return ans;
