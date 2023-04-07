06774a089c8996d7cc79c626ec35ee9a5576039b
Sohan Pulluru <sohanpulluru@gmail.com>
2023-04-07 23:52:06 +0530

    Create Add Minimum Characters gfg.cpp

---
+++ APR 2023/07 Apr/Add Minimum Characters gfg.cpp
@@ -0,0 +1,40 @@
+class Solution{   
+public:
+    int addMinChar(string str)
+    {
+        int n = str.length(); 
+        string rev = str; 
+        reverse(rev.begin(), rev.end()); 
+        string s = str + "#" + rev; 
+        int lps = computeLPS(s); 
+        return (n - lps); 
+        }
+    
+int computeLPS(string s) 
+{ 
+    int n = s.length(); 
+    int lps[n]; 
+    lps[0] = 0; 
+    int i = 1, j = 0; 
+    while (i < n) 
+    { 
+        if (s[i] == s[j]) 
+        { 
+            lps[i] = j + 1; 
+            i++; 
+            j++; 
+        } 
+        else
+        { 
+            if (j != 0) 
+                j = lps[j-1]; 
+            else
+            { 
+                lps[i] = 0; 
+                i++; 
+            } 
+        } 
+    } 
+    return lps[n-1]; 
+    }
+};
